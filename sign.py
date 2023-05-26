b7c95c6a1d07ad90f674cc66d4f4df85293e4bb5
Waman22 <wamaphaswna@gmail.com>
2023-05-26 14:52:37 +0200

    Project ready

--- sign.py
+++ sign.py
@@ -343,4 +343,4 @@ def TimeData(row, username):
     return render_template('TimeData.html', row=row, username=username)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
